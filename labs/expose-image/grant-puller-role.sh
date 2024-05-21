fcf138fa42bca520110b1c16d1e260b8ea2d5e5e
Marck <marc@github.com>
2024-05-21 17:51:26 +0200

    changed vars around

--- labs/expose-image/grant-puller-role.sh
+++ labs/expose-image/grant-puller-role.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-source /usr/local/etc/ocp4.config
-
+#source /usr/local/etc/ocp4.config
+RHT_OCP4_DEV_USER=marcknl 
 oc policy add-role-to-group  \
-    -n ${RHT_OCP4_DEV_USER}-common system:image-puller \
-    system:serviceaccounts:${RHT_OCP4_DEV_USER}-expose-image 
+    -n ${RHT_OCP4_DEV_USER}-dev system:image-puller \
+    system:serviceaccounts:${RHT_OCP4_DEV_USER}-dev-expose-image 
 
--- labs/post-commit/oc-new-app.sh
+++ labs/post-commit/oc-new-app.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source /usr/local/etc/ocp4.config
+RHT_OCP4_GITHUB_USER=Marck
+#source /usr/local/etc/ocp4.config
 oc new-app  --as-deployment-config --name hook \
-    php:7.3~http://github.com/${RHT_OCP4_GITHUB_USER}/DO288-apps \
+    php:7.3~http://github.com/${RHT_OCP4_GITHUB_USER}/study-DO288-apps \
     --context-dir post-commit
